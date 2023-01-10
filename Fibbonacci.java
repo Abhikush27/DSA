0a2c86d3138090d22535e310e71f7f18d5f6a563
Abhijeet Chawla <102854258+Abhikushchawla@users.noreply.github.com>
2023-01-11 00:33:27 +0530

    Update Fibbonacci.java

--- Fibbonacci.java
+++ Fibbonacci.java
@@ -6,27 +6,63 @@ public class Fibbonacci {
 //	In dynamic programming we use to store the values of the elements that are being repeated continuously 
 
 	
-	public static int fibo(int n ) {
+		
+//	METHOD 1
+//	Using recursion
+	public static int fibo1(int n) {
+		if(n==0 || n==1)
+			return n;
+		
+		return fibo1(n-1) + fibo1(n-2);
+	}
+	
+	
+	
+// METHOD 2	
+//	Using recursion+memorization
+	public static int fibo2(int n) {
+		
+//		step1 creating and filling the array
 		int []dp = new int[n+1];
 		Arrays.fill(dp, -1);
 		
-//		base case
 		if(n<=1)
 			return n;
 		
+//		step3 base case 
 		if(dp[n]!=-1)
 			return dp[n];
 		
-		dp[n] = fibo(n-1)+fibo(n-2);
+//		step2 filling the values in array
+		dp[n] = fibo2(n-1)+fibo2(n-2);
 		return dp[n];
 	}
+	
+	
+//	METHOD 3
+//	Tabulation
+	
+	public static int fibo3(int n) {
+//		step 1
+		int []dp = new int[n+2]; //doubt
+//		step 2
+		dp[1]=1;
+		dp[0]=0;
+		
+		if(n==0)
+			return 0;
+		
+//		step3
+		for(int i=2;i<=n;i++) {
+			dp[i] = dp[i-1]+dp[i-2];
+		}
+		return dp[n];
+	}
+	
 	public static void main(String[] args) {
 		
-		int x =fibo(8);
+		int x =fibo3(8);
 		System.out.print(x);
 	}
-//using the simple recursion it will take the time complexity of 2^n
-//	using dp it will take (n)
-//	space complexity of the above solution will be [O(n)+O(n)]
-	
+
 }
