7ec9e19c0d1b916bc49f0c9a85964a33fe3c5e77
Abhijeet Chawla <102854258+Abhikushchawla@users.noreply.github.com>
2023-03-26 01:52:27 +0530

    Update SquareRoot.java

--- SquareRoot.java
+++ SquareRoot.java
@@ -2,6 +2,40 @@ package BinarySearch;
 
 public class SquareRoot {
 
+	public long search(int x){
+	    long low =0;
+	     long high = x;
+	     long  mid = low+(high-low)/2;
+	long ans=-1;
+	     while(low<=high){
+	         long sqr = mid*mid;
+	         if(sqr==x)
+	         return mid;
+	         else if(sqr<x){
+	         ans=mid;
+	         low = mid+1;}
+	         else
+	         high=mid-1;
+
+	         mid = low+(high-low)/2;
+	     }
+	     return ans;
+	}
+	
+//	important
+	
+	public double afterDecimal(int x, int precision, int searchval) {
+		double factor =1;
+		double result = searchval;
+		for(int i=0;i<precision;i++) {
+			factor=factor/10;
+			
+			for(double j = searchval;j*j<x;j+=factor) {
+				result = j;
+			}
+		}
+		return result;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
